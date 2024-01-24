31ec3d2fe03cfe6f9631901bfead3cb5edc5890a
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-01-24 09:06:54 +0000

    added jquery function for save and display input data

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let currentDayNow = dayjs().format('dddd, MMMM D, YYYY');
-
 let currentDay = document.getElementById("currentDay").innerText = currentDayNow;
 
-function saveInput() {
-    let inputTaskText = document.getElementById("inputTaskText").value;
-    let saveInputTaskText = localStorage.setItem("inputTaskText", inputTaskText);
-}
+$(document).ready (function () {
+    $("*[data-content]").each(function () {
+      $(this).val(localStorage.getItem("item-" + $(this).attr("data-content")));
+    });
+
+    $("*[data-content]").on("keyup", function (itm) {
+      localStorage.setItem ("item-" + $(this).attr("data-content"), $(this).val());
+    })
+  })
