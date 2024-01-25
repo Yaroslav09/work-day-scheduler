81685040c11511e47f3ea2a95d7e35d1c55ac4e4
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-01-25 00:35:49 +0000

    added background color for textarea, updated README file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # work-day-scheduler
+
+## User story
+
+* AS AN employee with a busy schedule
+    - I WANT to add important events to a daily planner
+- SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+
+* The app should:
+    - Display the current day at the top of the calendar when a user opens the planner.
+    - Present timeblocks for standard business hours when the user scrolls down.
+    - Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+    - Allow a user to enter an event when they click a timeblock.
+    - Save the event in local storage when the save button is clicked in that timeblock.
+    - Persist events between refreshes of a page.
+
+![image](./work_day_scheduler.jpg)
+
+## Links
+
+GitHub Repositories: https://github.com/Yaroslav09/work-day-scheduler
+
+Deployed Application: https://yaroslav09.github.io/work-day-scheduler/
--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 let currentDayNow = dayjs().format('dddd, MMMM D, YYYY');
 let currentDay = document.getElementById("currentDay").innerText = currentDayNow;
 
+
+function backgroundAllHours() {
+    let hour = dayjs().format('HH');
+    let listHours = document.querySelectorAll("[data-content]");         
+
+    listHours.forEach(function(element) {      
+      let dataValue = element.getAttribute('data-content');
+      let textareaBackground = document.querySelector(`textarea[data-content="${dataValue}"]`);
+
+      if (dataValue < hour) {  
+        if (textareaBackground) {
+          textareaBackground.classList.add("past");
+        }
+      }
+      if (dataValue === hour) {                    
+        if (textareaBackground) {
+          textareaBackground.classList.add("present");
+        }
+      }
+
+      if (dataValue > hour) { 
+        if (textareaBackground) {
+          textareaBackground.classList.add("future");
+        }
+      }       
+    });    
+}
+backgroundAllHours();
+
 $(document).ready (function () {
     $("*[data-content]").each(function () {
       $(this).val(localStorage.getItem("item-" + $(this).attr("data-content")));
